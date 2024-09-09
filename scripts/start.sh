72b5be8a8a6fabe7e351900b31cc84e51692a971
Brody <brodyover@gmail.com>
2024-09-08 21:50:22 -0400

    xvfb

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+export DISPLAY=:99
+
 parallel --ungroup --halt now,done=1 ::: \
+    "./start_xvfb.sh" \
     "./start_browserless.sh" \
     "./start_caddy.sh"
 
---
+++ scripts/start_xvfb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec Xvfb $DISPLAY -screen 0 1920x1080x24 -nolisten tcp -nolisten unix
